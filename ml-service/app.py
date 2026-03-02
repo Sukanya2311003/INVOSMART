3a5e232806fe1354223491ed0e5d27d83383cb34
Sukanya <karnsukanya@gmail.com>
2026-03-02 18:10:25 +0530

    Added requirement.txt file for deployment

--- ml-service/app.py
+++ ml-service/app.py
@@ -43,5 +43,7 @@ def predict():
         "risk_level": risk_level
     })
 
+# if __name__ == "__main__":
+#     app.run(port=5000, debug=True)
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
